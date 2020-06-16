bc9c41a92104397bded3492befc6c3cc9d9438a2
何孔祥 <c-hekongxiang@genomics.cn>
2020-06-16 15:14:13 +0800

    init

--- src/main/java/com/jump/ReportApplication.java
+++ src/main/java/com/jump/ReportApplication.java
@@ -1,8 +1,17 @@
 package com.jump;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Jump
  * @date 2020/6/16 14:11
  */
+@SpringBootApplication
 public class ReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportApplication.class, args);
+    }
+
 }
